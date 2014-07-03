e6416504437ce899abfe1bf0a5e76fd255c81fbd
pocketkk <pocketkk@gmail.com>
2014-07-03 14:19:15 -0700

    Initial Commit

---
+++ coldcall/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  coldcall
+//
+//  Created by Jason Crump on 7/3/14.
+//  Copyright (c) 2014 Jason Crump. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
